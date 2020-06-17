50a43463b7c9ce2b37af2e34d39363ee147eea36
Túlio Freitas <tuliofreitasgomes@gmail.com>
2020-06-17 13:14:56 -0300

    Building a GraphQL API

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,77 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-const app = express();
+// Run the server on a port specified in our .env file or port 4004
 const port = process.env.PORT || 4004;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+let books = [
+	{
+		id: '1',
+		title: 'This is book title', 
+		author: 'John Doe'
+	},
+	{
+		id: '2',
+		title: 'This is another book title', 
+		author: 'John Doe'
+	},
+	{
+		id: '3',
+		title: 'Oh hey look, another book title', 
+		author: 'John Doe'
+	}
+]
 
-app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+	type Book {
+		id: ID!
+		title: String!
+		author: String!
+	}
+  type Query {
+    hello: String!
+    books: [Book!]!
+    book(id: ID!): Book!
+  }
+  type Mutation {
+    newBook(title: String!): Book
+  }
+`;
+
+// Provide resolver functions for our schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!', 
+    books: () => books, 
+    book: (parent, args) => {
+    	return books.find(book => book.id === args.id)
+    }
+  }, 
+  Mutation: {
+    newBook: (parent, args) => {
+      let bookValue = {
+        id: String(books.length + 1),
+        title: args.title,
+        author: 'John Doe'
+      };
+      books.push(bookValue);
+      return bookValue;
+    }
+  }
+};
+
+const app = express();
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
 );
